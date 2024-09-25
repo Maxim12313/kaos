4b20b80b2f049933aa28b9248ed0cef31076a7a2
maxim12313 <maximkim16@gmail.com>
2024-09-25 16:52:17 -0400

    took out name from website

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,8 @@
 export default function Footer() {
   return (
     <footer className="w-full h-10 flex flex-col justify-center border-t-2 border-myslate">
-      <div className="h-full w-full flex flex-row justify-between items-center px-16 text-sm">
+      <div className="h-full w-full flex flex-row justify-center items-center px-16 text-sm">
         <p>&copy; 2024 Korean American Otolargyngology Society</p>
-        <a
-          href="https://github.com/Maxim12313/kaos"
-          className="underline underline-offset-2"
-        >
-          Created By Maxim Kim
-        </a>
       </div>
     </footer>
   );
